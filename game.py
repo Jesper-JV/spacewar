934a63fb185f45789923c7c49cd52c4e7ee29e0d
Jesper <jesper@gmail.com>
2025-04-29 01:25:26 +0200

    added new variables to track current wave also added player health

--- game.py
+++ game.py
@@ -8,26 +8,29 @@ playercenter = 24
 lo_collected = 0
 playerstep = 5
 enemyspawns = 2
+enemyhealth = 2
+playerhealth = 3
+current_wave = 1
 enemyimage = "images/enemy_green.png"
 with open("highscore.txt", "r") as f:
     highscore = int(f.read())
 loadout_inbound_sound = False
 loadout_is_collected = False
-hitboxx = 40
-game_status = "ongoing"
-spaceship_img = "images/spaceship.png"
-loadoutsrn = False
-player1_points = 0
-hitboxy = 30
-green = (0,255,0)
-white = (255,255,255)
-red = (255,0,0)
-x = 400
-y = 700
-last_shot_time = 0
-cooldown = 400
-boss_spawned = False
-sound_played = False
+hitboxx = 40 
+game_status = "ongoing" 
+spaceship_img = "images/spaceship.png" # player image
+loadoutsrn = False # makes it only one loadout on the screen
+player1_points = 0 # amount of enemys player1 has destroyed
+hitboxy = 30 # hitbox (y) for enemy(s)
+green = (0,255,0) # color code for green
+white = (255,255,255) # color code for white
+red = (255,0,0) # color code for red
+x = 400 # width of the frame
+y = 700 # height of the frame
+last_shot_time = 0 # last time you shoot a bullet
+cooldown = 400 # cooldown between bullets
+boss_spawned = False # spawns boss once
+sound_played = False # plays sound once
 spawn_delay = 2000  # time in milliseconds between spawns
 last_spawn_time = pygame.time.get_ticks()  # current time
 
@@ -80,8 +83,11 @@ def loadout_rewards(playerstep):
     if lo_collected == 3:
         playerstep = 7
         cooldown = 200
-    if lo_collected >= 4:
+    if 5 > lo_collected >= 4:
         playerstep = 9
+        cooldown = 0
+    if lo_collected >= 5:
+        playerstep = 10
         cooldown = 0
     return playerimage,playercenter,cooldown,playerstep  
     
@@ -100,7 +106,9 @@ class Enemy():
         screen.blit(self.image, (self.x,self.y))
     def movement(self):
         
-        self.y += self.steps 
+        self.y += self.steps
+
+
     def detection(self):
 
         for bullet in bulletlist:
@@ -126,20 +134,30 @@ class Bullet():
         self.y -= self.speed
 
 pygame.init()
+# Create frame
 screen = pygame.display.set_mode((x, y))
+# Set caption
 pygame.display.set_caption("SPACEWAR VER 1.0.0")
+# Detect time
 clock = pygame.time.Clock()
+# Load text
 font1 = pygame.font.Font('freesansbold.ttf', 50)
 font2 = pygame.font.Font('freesansbold.ttf', 20)
 font3 = pygame.font.Font('freesansbold.ttf', 40)
+playerhealth1 = pygame.font.Font('freesansbold.ttf', 20)
 highscore1 = pygame.font.Font('freesansbold.ttf', 20)
+waves_passed = pygame.font.Font('freesansbold.ttf', 20)
+# Text and color for text
 text1= font1.render('YOU WIN', True,green)
 text3= font3.render('WARZONE DEFEAT', True,red)
-highscore1 = highscore1.render('highscore: ' + str(highscore), True,white)
 
+highscore1 = highscore1.render('Highscore: ' + str(highscore), True,white)
+
+
+# Set center for text
 text1_rect = text1.get_rect(center=(x // 2, y // 2))
 text3_rect = text3.get_rect(center=(x // 2, y // 2))
-highscore_rect = highscore1.get_rect(center=(330,30))
+highscore_rect = highscore1.get_rect(center=(310,30))
 
 # Load images
 
@@ -159,7 +177,7 @@ playerx = x // 2
 playery = y - (y // 4)
 bulletlist = []
 enemylist = []
-playerx += 5
+
 for i in range(2):
     enemylist.append(Enemy("images/enemy_green.png",2,False))
 
@@ -182,8 +200,9 @@ while True:
 
     if current_time - last_spawn_time > spawn_delay:
         for i in range(enemyspawns):
-            enemylist.append(Enemy(enemyimage, 2,False))
+            enemylist.append(Enemy(enemyimage, enemyhealth,False))
         last_spawn_time = current_time
+        current_wave += 1
     
     playerx,playery = player_movement(playerx,playery,playerstep)
 
@@ -194,11 +213,19 @@ while True:
     screen.blit(background, (0, - scroll_y))
     screen.blit(background,(0,bg_height - scroll_y))
     
-    text2 = font2.render("points: " + str(player1_points),True,white)
+    text2 = font2.render("Points: " + str(player1_points),True,white)
+    playerhealth1_text = playerhealth1.render( "Health: "+ str(playerhealth), True,white)
     text2_rect = text2.get_rect(center=(50,30))
+    playerhealth1_text_rect = playerhealth1_text.get_rect(center=(50,80))
+    waves_passed_text = waves_passed.render('Current wave: ' + str(current_wave), True,white)
+      
+    waves_passed_text_rect = waves_passed_text.get_rect(center=(82,130))
 
+    screen.blit(waves_passed_text, waves_passed_text_rect)
     screen.blit(highscore1, highscore_rect)
     screen.blit(text2, text2_rect)
+    screen.blit(playerhealth1_text, playerhealth1_text_rect)
+    
 
     # Draw player (needed so it still shows when no keys pressed)
     #screen.blit(playerimage, (playerx, playery))
@@ -206,10 +233,14 @@ while True:
         if (enemy.x < playerx + 50 and
             enemy.x + 40 > playerx and  
             enemy.y < playery + 53 and  
-            enemy.y + 29 > playery):    
-            game_status = "loss"
-            update_highscore(player1_points)
+            enemy.y + 29 > playery):  
+            playerhealth -= 1
+            enemylist.remove(enemy)  
             
+    if playerhealth == 0:
+        game_status = "loss"
+        update_highscore(player1_points)
+
     if game_status == "loss":
         screen.blit(text3, text3_rect)
     
@@ -222,6 +253,9 @@ while True:
     for enemy in enemylist:
         enemy.image_blit(screen)
         enemy.movement()
+        if enemy.y > 700:
+            enemylist.remove(enemy)
+            
         
     for bullet in bulletlist:
         bullet.image_blit(screen)
@@ -244,29 +278,36 @@ while True:
                     else:
                         explosion_sound.play()
                         player1_points += 1
-                        lo_spawn = random.choice([7])
+                        lo_spawn = random.choice([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
                         #makes it 10 percent chance for loadout every time you kill an enemy
                         if lo_spawn == 7 and loadoutsrn == False:
                             loadoutsrn = True
                             loadout_inbound.play() 
                             loadout = pygame.image.load("images/loadout.png").convert_alpha()
                 break
-    # pdb.set_trace()
-    if 100 > player1_points >= 25:
-        enemyspawns = 4
 
-    if 300 > player1_points >= 100:
-        enemyspawns = 10
+    if 100 > player1_points >= 25:
+        enemyspawns = 3
+        
+
+    if 150 > player1_points >= 100:
+        enemyspawns = 5
         enemyimage = "images/enemy_red.png"
 
     if 250 > player1_points > 150:
-        spawn_delay = 1000
+        spawn_delay = 1500
+        enemyspawns = 8
 
-    if player1_points > 250:
-        spawn_delay = 500
+    if 500 > player1_points > 250:
+        spawn_delay = 1000
     
-    if player1_points > 300:
-        enemy_spawns = 15
+    if 750 > player1_points >= 300:
+        enemyspawns = 10
+        enemyimage = "images/red_final .png"
+
+    if player1_points > 500:
+        enemyhealth = 3
+        spawn_delay = 500
 
     if loadoutsrn == True:      
         screen.blit(loadout, (loadoutx, loadouty))
@@ -282,11 +323,11 @@ while True:
             loadouty + 29 > playery):
             loadout_collected.play() 
             loadoutsrn = False 
-            lo_collected += 1  
+            lo_collected += 1
+            playerhealth += 1  
             loadoutx =random.randint(0,380)
             loadouty = -30
             if lo_collected >= 1:  
-                pdb.set_trace
                 playerimage,playercenter,cooldown,playerstep = loadout_rewards(playerstep)
        
     if player1_points >= 10:
